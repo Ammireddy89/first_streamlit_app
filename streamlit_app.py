8ba9571aa5067ae49fcf49e0e9d3756d898b729a
Ammireddy89 <149769965+Ammireddy89@users.noreply.github.com>
2023-11-03 14:34:41 +0530

    Update streamlit_app.py
    Added pics

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
-streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry oatmeal')
-streamlit.text('Kale,Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.title('🥣 My Parents New Healthy Diner')
+streamlit.header(' 🥗 Breakfast Menu')
+streamlit.text(' 🐔 Omega 3 & Blueberry oatmeal')
+streamlit.text(' 🥑Kale,Spinach & Rocket Smoothie')
+streamlit.text('🍞Hard-Boiled Free-Range Egg')
